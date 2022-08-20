c3050b3482b78cb9c520d7635e04065ad399a22a
cigamsix <cigamsix@gmail.com>
2022-08-20 18:25:59 +0900

    change everything to neko chan

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
-import reactLogo from "../assets/react.svg"
-
+import reactLogo from "../assets/kitty.png"
 
 export default function Navbar() {
     return (
         <nav>
             <img src={reactLogo} className="nav--icon" />
-            <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            <h3 className="nav--logo_text">ねこ ちゃん ! !</h3>
+            <h4 className="nav--title">Neko chan website</h4>
         </nav>
     )
 }
