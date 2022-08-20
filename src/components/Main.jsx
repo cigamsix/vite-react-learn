ff7b93f3c84b0ce66e54f4c4dad2daaaedd2aa56
cigamsix <cigamsix@gmail.com>
2022-08-20 19:36:56 +0900

    add yellow low half opacity cat to the background

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,13 +5,15 @@ export default function Main() {
     return (
         <main>
             <h1 className="main--title">Fun facts about React</h1>
+            <div class="hero">
+            </div>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
-                <li>Just want to try another fact</li>
+                <li>Just want to try another facts</li>
             </ul>
         </main>
     )
