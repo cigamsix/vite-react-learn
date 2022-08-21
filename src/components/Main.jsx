d00596b9032cdfb439b6e594b813675bb910251a
cigamsix <cigamsix@gmail.com>
2022-08-21 20:59:37 +0900

    first commit as react learn

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,29 +3,14 @@ import React from "react"
 export default function Main() {
     return (
         <main>
-            <h1 className="main--title">猫から学んだ 7 のこと
-            <br/><sub>7 things I learned from cats</sub>
-            </h1>
-            
-            <div class="hero">
-            </div>
-            
-            <ol className="main--facts">
-                <li>適度な距離感を保つ               
-                    <br/><small>keep an appropriate distance</small></li> 
-                <li>居心地の良い場所を、自ら見つける  
-                    <br/><small>find a comfortable place</small></li>
-                <li>今を生きる         
-                    <br/><small>live in the present</small></li>
-                <li>ちゃんと充電する                     
-                    <br/><small>sleep to get enough power</small></li>
-                <li>幸せそうに寝る                      
-                    <br/><small>sleep happily</small></li>
-                <li>周りの世界に興味を持つ              
-                    <br/><small>find something interesting</small></li>
-                <li>イヤなことからちゃんと逃げる        
-                    <br/><small>run away from unpleasant things</small></li>
-            </ol>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
         </main>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import catLogo from "../assets/kitty.png"
+import reactLogo from "../assets/react.svg"
 
 export default function Navbar() {
     return (
         <nav>
-            <img src={catLogo} className="nav--icon" />
-            <h3 className="nav--logo_text">ねこ ちゃん ! !</h3>
-            <h4 className="nav--title">Neko chan website</h4>
+            <img src={reactLogo} className="nav--icon" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
     )
 }
